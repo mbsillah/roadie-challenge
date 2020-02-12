bcfc8839dcd8a11bcb26539f0f49820a8410bca6
sillah2010 <macktastic2014@gmail.com>
2020-02-12 12:46:37 -0500

    fake data test

--- api/reviews.js
+++ api/reviews.js
@@ -1,4 +1,36 @@
 module.exports = (req, res) => {
-  const { name = "World" } = req.query;
-  res.status(200).send(`Hello ${name}!`);
+  res.json([
+    {
+      title: "Perfect",
+      rating: 5,
+      user: "User 1",
+      date: "01/01/20",
+      review:
+        "Lorem Khaled Ipsum is a major key to success. Look at the sunset, life is amazing, life is beautiful, life is what you make it. The key to more success is to get a massage once a week, very important, major key, cloth talk. Wraith talk. It’s important to use cocoa butter. It’s the key to more success, why not live smooth? Why live rough?"
+    },
+    {
+      title: "Great device!",
+      rating: 4,
+      user: "User 2",
+      date: "01/01/20",
+      review:
+        "Look at the sunset, life is amazing, life is beautiful, life is what you make it. The first of the month is coming, we have to get money, we have no choice. It cost money to eat and they don’t want you to eat."
+    },
+    {
+      title: "Easy installation",
+      rating: 4,
+      user: "User 3",
+      date: "01/01/20",
+      review:
+        "Every chance I get, I water the plants, Lion! Major key, don’t fall for the trap, stay focused. It’s the ones closest to you that want to see you fail. You see the hedges, how I got it shaped up? It’s important to shape up your hedges, it’s like getting a haircut, stay fresh."
+    },
+    {
+      title: "Not bad",
+      rating: 3,
+      user: "User 4",
+      date: "01/01/20",
+      review:
+        " Watch your back, but more importantly when you get out the shower, dry your back, it’s a cold world out there. The first of the month is coming, we have to get money, we have no choice. It cost money to eat and they don’t want you to eat."
+    }
+  ]);
 };
