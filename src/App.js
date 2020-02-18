ea585aa5206628efc01797134038361678521a68
sillah2010 <macktastic2014@gmail.com>
2020-02-17 20:20:57 -0500

    Minor tweak to filter

--- src/App.js
+++ src/App.js
@@ -11,52 +11,6 @@ import "./App.css";
 Modal.setAppElement("#root");
 
 function App() {
-  // const [reviews, setReviews] = useState({
-  //   originalReviews: [
-  //     {
-  //       title: "Perfect",
-  //       rating: 5,
-  //       user: "User 1",
-  //       date: "01/01/2020",
-  //       review:
-  //         "Lorem Khaled Ipsum is a major key to success. Look at the sunset, life is amazing, life is beautiful, life is what you make it. The key to more success is to get a massage once a week, very important, major key, cloth talk. Wraith talk. It’s important to use cocoa butter. It’s the key to more success, why not live smooth? Why live rough?"
-  //     },
-  //     {
-  //       title: "Great device!",
-  //       rating: 4,
-  //       user: "User 2",
-  //       date: "01/01/2020",
-  //       review:
-  //         "Look at the sunset, life is amazing, life is beautiful, life is what you make it. The first of the month is coming, we have to get money, we have no choice. It cost money to eat and they don’t want you to eat."
-  //     },
-  //     {
-  //       title: "Easy installation",
-  //       rating: 4,
-  //       user: "User 3",
-  //       date: "01/01/2020",
-  //       review:
-  //         "Every chance I get, I water the plants, Lion! Major key, don’t fall for the trap, stay focused. It’s the ones closest to you that want to see you fail. You see the hedges, how I got it shaped up? It’s important to shape up your hedges, it’s like getting a haircut, stay fresh."
-  //     },
-  //     {
-  //       title: "Not bad",
-  //       rating: 3,
-  //       user: "User 4",
-  //       date: "01/01/2020",
-  //       review:
-  //         " Watch your back, but more importantly when you get out the shower, dry your back, it’s a cold world out there. The first of the month is coming, we have to get money, we have no choice. It cost money to eat and they don’t want you to eat."
-  //     }
-  //   ],
-  //   filterReviews: [],
-  //   average: 0,
-  //   percentages: {
-  //     "1": 0,
-  //     "2": 0,
-  //     "3": 0,
-  //     "4": 0,
-  //     "5": 0
-  //   }
-  // });
-
   const [modal, setModal] = useState(false);
   const [filter, setFilter] = useState({
     isActive: false,
@@ -122,13 +76,11 @@ function App() {
         </span>
       </div>
     );
-    // console.log("review Total: ",reviews.reduce((acc, cv) => acc + cv.rating, 0))
-    // console.log("review Total Amount: ",reviews.length)
   };
 
   const filterReviews = (e, id) => {
     e.preventDefault();
-    if (filter.isActive) {
+    if (filter.isActive && filter.filterBy === id) {
       setReviews({
         ...reviews,
         filterReviews: []
@@ -166,16 +118,16 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    async function getReviews() {
-      const res = await axios.get("/api/reviews");
-      setReviews({
-        ...reviews,
-        originalReviews: res.data
-      });
-    }
-    getReviews();
-  }, []);
+  // useEffect(() => {
+  //   async function getReviews() {
+  //     const res = await axios.get("/api/reviews");
+  //     setReviews({
+  //       ...reviews,
+  //       originalReviews: res.data
+  //     });
+  //   }
+  //   getReviews();
+  // }, []);
 
   useEffect(() => {
     calculateReviewAverages();
